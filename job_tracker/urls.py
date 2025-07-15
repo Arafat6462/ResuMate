6e1bb1f176f50f03accfbae1f332a4f669e11004
Arafat Hossain <arafat6462@gmail.com>
2025-07-15 20:15:20 +0600

    Add ExampleJobApplicationViewSet and update JobApplicationViewSet permissions

--- job_tracker/urls.py
+++ job_tracker/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import JobApplicationViewSet
+from .views import JobApplicationViewSet, ExampleJobApplicationViewSet
 
 router = DefaultRouter()
-router.register(r'job-applications', JobApplicationViewSet)
+router.register(r'job-applications', JobApplicationViewSet, basename='jobapplication')
+router.register(r'example-job-applications', ExampleJobApplicationViewSet, basename='examplejobapplication')
 
 urlpatterns = [
     path('', include(router.urls)),
--- job_tracker/views.py
+++ job_tracker/views.py
@@ -1,7 +1,39 @@
-from rest_framework import viewsets
+from rest_framework import viewsets, permissions
+
 from .models import JobApplication
 from .serializers import JobApplicationSerializer
 
+
 class JobApplicationViewSet(viewsets.ModelViewSet):
-    queryset = JobApplication.objects.all()
     serializer_class = JobApplicationSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get_queryset(self):
+        """
+        This view should return a list of all the job applications
+        for the currently authenticated user.
+        """
+        return self.request.user.jobapplication_set.filter(is_deleted=False)
+
+    def perform_create(self, serializer):
+        """Associate the user with the job application."""
+        serializer.save(user=self.request.user)
+
+    def perform_destroy(self, instance):
+        """Soft delete the job application."""
+        instance.is_deleted = True
+        instance.save()
+
+
+class ExampleJobApplicationViewSet(viewsets.ReadOnlyModelViewSet):
+    """
+    A viewset that provides 5 sample job applications for anonymous users.
+    """
+    serializer_class = JobApplicationSerializer
+    permission_classes = [permissions.AllowAny]
+
+    def get_queryset(self):
+        """
+        This view should return a list of 5 sample job applications.
+        """
+        return JobApplication.objects.filter(is_example=True)[:5]
