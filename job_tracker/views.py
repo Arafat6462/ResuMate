eef24bb6d9a85a6c06ebe39a2e7e6949f820cadc
Arafat Hossain <arafat6462@gmail.com>
2025-08-03 12:28:25 +0600

    Implement caching for example job applications to reduce database load

--- job_tracker/views.py
+++ job_tracker/views.py
@@ -1,4 +1,6 @@
+from django.core.cache import cache
 from rest_framework import viewsets, permissions
+from rest_framework.response import Response
 
 from .models import JobApplication
 from .serializers import JobApplicationSerializer
@@ -28,12 +30,32 @@ class JobApplicationViewSet(viewsets.ModelViewSet):
 class ExampleJobApplicationViewSet(viewsets.ReadOnlyModelViewSet):
     """
     A viewset that provides 5 sample job applications for anonymous users.
+    This view is cached for 24 hours to reduce database load.
     """
     serializer_class = JobApplicationSerializer
     permission_classes = [permissions.AllowAny]
+    CACHE_KEY = "example_job_applications"
+    CACHE_TIMEOUT = 60 * 60 * 24  # 24 hours
 
     def get_queryset(self):
         """
         This view should return a list of 5 sample job applications.
         """
         return JobApplication.objects.filter(is_example=True)[:5]
+
+    def list(self, request, *args, **kwargs):
+        # Try to get the data from the cache first
+        cached_data = cache.get(self.CACHE_KEY)
+        if cached_data:
+            # Cache Hit: Return cached data with a custom header
+            return Response(cached_data, headers={'X-Cache-Status': 'HIT'})
+
+        # Cache Miss: Fetch from DB
+        queryset = self.get_queryset()
+        serializer = self.get_serializer(queryset, many=True)
+        
+        # Save the serialized data to the cache for next time
+        cache.set(self.CACHE_KEY, serializer.data, self.CACHE_TIMEOUT)
+        
+        # Return new data with a custom header
+        return Response(serializer.data, headers={'X-Cache-Status': 'MISS'})
